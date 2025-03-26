d4f883079b21b9b3f617e266b2e5796d9bf9e2ac
Guac0 <YeetYeetGuac@gmail.com>
2025-03-25 20:48:00 -0400

    dt - fix time handling

--- doubletap/debian/doubletap.sh
+++ doubletap/debian/doubletap.sh
@@ -4,24 +4,35 @@ fuse_armed="false"      # DO NOT SET THIS TO TRUE UNLESS YOU WANT TO EXPLODE
 debug="false"           # 
 
 #########################
-###### TIME CHECK #######
+##### EXTRA SETUP #######
 #########################
+to_seconds() {
+    # Convert HH:MM:SS into total seconds
+    echo "$1" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }'
+}
+
 today=$(date +"%Y-%m-%d")
-current_hour=$(date +"%H")
+current_time=$(date +"%H:%M:%S")
+fuse_seconds=$(to_seconds "$fuse_time")
+current_seconds=$(to_seconds "$current_time")
 
 if [ "$debug" = "true" ]; then
     echo "---------------------------------------------"
-    echo "  Timebomb - $today $current_hour"
+    echo "  Timebomb - $today $current_time"
 fi
+
+#########################
+###### TIME CHECK #######
+#########################
 
 # Check if the date is March 21st, 2025, and the time is after 3 PM
 if [ "$today" = "$fuse_date" ]; then
-    if [ "$fuse_time" -ge $current_hour ]; then
+    if [ "$fuse_seconds" -ge "$current_seconds" ]; then
         if [ "$debug" = "true" ]; then
             echo "+++++++++++++++++++++++++++++++++++"
             echo "  DETONATION CRITERIA MET"
             echo "Found date $today and expected $fuse_date"
-            echo "Found hour $current_hour and expected $fuse_hour"
+            echo "Found hour $current_time and expected $fuse_time"
             if [ "$fuse_armed" = "true" ]; then 
                 echo "  PROCEEDING WITH FULL KABOOM"
             else
@@ -35,10 +46,12 @@ if [ "$today" = "$fuse_date" ]; then
             # Delete persistance/kill this task
             # doesn't matter if blue team finds this because they can just delete this file
             # todo: unless this file is stabvested. stabvest should contain an auto-off after the fuse time
+            # placeholder:
+            :
         fi
     else 
         if [ "$debug" = "true" ]; then
-            echo "Wrong hour - found $today but expected $current_hour"
+            echo "Wrong hour - found $today but expected $current_time"
         fi
     fi
 else
